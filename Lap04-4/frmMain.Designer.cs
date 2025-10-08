045cf8ab3d7a2c93219c49b2b69118b6e46f875f
Nguyexn Dii <nguyenduy.hd.0411@gmail.com>
2025-10-08 19:32:26 +0700

    xong lap04-4

---
+++ Lap04-4/frmMain.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Lap04_4
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkAllInMonth = new System.Windows.Forms.CheckBox();
+            this.lblDeliveryTime = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpTo);
+            this.groupBox1.Controls.Add(this.lblTo);
+            this.groupBox1.Controls.Add(this.lblDeliveryTime);
+            this.groupBox1.Controls.Add(this.dtpFrom);
+            this.groupBox1.Controls.Add(this.chkAllInMonth);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 142);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin đơn hàng";
+            // 
+            // chkAllInMonth
+            // 
+            this.chkAllInMonth.AutoSize = true;
+            this.chkAllInMonth.Location = new System.Drawing.Point(33, 40);
+            this.chkAllInMonth.Name = "chkAllInMonth";
+            this.chkAllInMonth.Size = new System.Drawing.Size(198, 24);
+            this.chkAllInMonth.TabIndex = 0;
+            this.chkAllInMonth.Text = "Xem tất cả trong tháng";
+            this.chkAllInMonth.UseVisualStyleBackColor = true;
+            // 
+            // lblDeliveryTime
+            // 
+            this.lblDeliveryTime.AutoSize = true;
+            this.lblDeliveryTime.Location = new System.Drawing.Point(29, 84);
+            this.lblDeliveryTime.Name = "lblDeliveryTime";
+            this.lblDeliveryTime.Size = new System.Drawing.Size(165, 20);
+            this.lblDeliveryTime.TabIndex = 1;
+            this.lblDeliveryTime.Text = "Thời gian giao hàng từ";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Location = new System.Drawing.Point(200, 84);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(200, 26);
+            this.dtpFrom.TabIndex = 1;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Location = new System.Drawing.Point(449, 84);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(200, 26);
+            this.dtpTo.TabIndex = 1;
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(406, 89);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(36, 20);
+            this.lblTo.TabIndex = 1;
+            this.lblTo.Text = "đến";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvOrders);
+            this.panel1.Location = new System.Drawing.Point(12, 160);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(777, 252);
+            this.panel1.TabIndex = 1;
+            // 
+            // dgvOrders
+            // 
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvOrders.Location = new System.Drawing.Point(0, 0);
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.RowHeadersWidth = 62;
+            this.dgvOrders.RowTemplate.Height = 28;
+            this.dgvOrders.Size = new System.Drawing.Size(777, 252);
+            this.dgvOrders.TabIndex = 0;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(497, 421);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(87, 20);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Tổng Cộng";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(590, 418);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(199, 26);
+            this.txtTotal.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblTotal);
+            this.Name = "frmMain";
+            this.Text = "Đơn Hàng";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblDeliveryTime;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.CheckBox chkAllInMonth;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvOrders;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ Lap04-4/frmMain.cs
@@ -0,0 +1,99 @@
+﻿using Lap04_4.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Lap04_4
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //hiện thị ngày tháng năm hiện tại
+            dtpFrom.Format = DateTimePickerFormat.Custom;
+            dtpFrom.CustomFormat = "dd/MM/yyyy";
+            dtpTo.Format = DateTimePickerFormat.Custom;
+            dtpTo.CustomFormat = "dd/MM/yyyy";
+
+            dtpFrom.Value = DateTime.Today;
+            dtpTo.Value = DateTime.Today;
+            chkAllInMonth.Checked = false;
+            txtTotal.ReadOnly = true;
+
+            // Tự động tìm kiếm lúc mở form
+            LoadGrid();
+
+            // Khi người dùng thay đổi -> tự cập nhật
+            dtpFrom.ValueChanged += (_, __) => LoadGrid();
+            dtpTo.ValueChanged += (_, __) => LoadGrid();
+            chkAllInMonth.CheckedChanged += (_, __) => LoadGrid();
+        }
+
+        private void LoadGrid()
+        {
+            // Nếu người dùng chọn sai khoảng thì hoán đổi cho hợp lệ
+            if (!chkAllInMonth.Checked && dtpFrom.Value.Date > dtpTo.Value.Date)
+            {
+                var t = dtpFrom.Value;
+                dtpFrom.Value = dtpTo.Value;
+                dtpTo.Value = t;
+            }
+
+            using (var db = new ProductOrderDBContext())
+            {
+                IQueryable<Invoice> q = db.Invoices;
+
+                if (chkAllInMonth.Checked)
+                {
+                    int y = dtpFrom.Value.Year;
+                    int m = dtpFrom.Value.Month;
+                    q = q.Where(i => i.DeliveryDate.Year == y && i.DeliveryDate.Month == m);
+                }
+                else
+                {
+                    DateTime from = dtpFrom.Value.Date;
+                    DateTime toEx = dtpTo.Value.Date.AddDays(1); // < toEx để bao trọn ngày To
+                    q = q.Where(i => i.DeliveryDate >= from && i.DeliveryDate < toEx);
+                }
+
+                var data = q
+                    .Select(i => new
+                    {
+                        i.InvoiceNo,
+                        i.OrderDate,
+                        i.DeliveryDate,
+                        Total = i.Orders.Sum(o => (decimal?)o.Price * o.Quantity) ?? 0
+                    })
+                    .OrderBy(x => x.DeliveryDate).ThenBy(x => x.InvoiceNo)
+                    .ToList()
+                    .Select((x, idx) => new
+                    {
+                        STT = idx + 1,
+                        SốHĐ = x.InvoiceNo,
+                        NgàyĐặtHàng = x.OrderDate,
+                        NgàyGiaoHàng = x.DeliveryDate,
+                        ThànhTiền = x.Total
+                    })
+                    .ToList();
+
+                dgvOrders.AutoGenerateColumns = true; // hoặc map cột thủ công nếu bạn đã tạo sẵn
+                dgvOrders.DataSource = data;
+
+                txtTotal.Text = (data.Sum(r => r.ThànhTiền)).ToString("N0");
+            }
+        }
+
+    }
+}
